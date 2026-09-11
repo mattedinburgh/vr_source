f03aa8998f3fa2f45b0616ddf14384dcb09fe1e4
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-11 22:46:14 +0200

    Launchers: fix mortar Heavy Weapons penalty scaling

--- Tactical/Weapons.cpp
+++ Tactical/Weapons.cpp
@@ -10939,7 +10939,7 @@ UINT32 CalcThrownChanceToHit(SOLDIERTYPE *pSoldier, INT32 sGridNo, INT16 ubAimTi
 				if ( Item[ usHandItem ].mortar )
 				{
 					if (HAS_SKILL_TRAIT( pSoldier, HEAVY_WEAPONS_NT ))
-						iChance += (gSkillTraitValues.sCtHModifierMortar * max( 0, ((100 - gSkillTraitValues.ubHWMortarCtHPenaltyReduction * NUM_SKILL_TRAITS( pSoldier, HEAVY_WEAPONS_NT ))/100)));
+						iChance += gSkillTraitValues.sCtHModifierMortar * max( 0, 100 - gSkillTraitValues.ubHWMortarCtHPenaltyReduction * NUM_SKILL_TRAITS( pSoldier, HEAVY_WEAPONS_NT ) ) / 100;
 					else
 						iChance += gSkillTraitValues.sCtHModifierMortar;
 				}
