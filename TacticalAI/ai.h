2e4c4a48340e07be27cfc1def87a0db47e31fd41
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-11 02:03:01 +0200

    AI: declare local advance support modifier

--- TacticalAI/ai.h
+++ TacticalAI/ai.h
@@ -294,6 +294,7 @@ BOOLEAN AICheckWeOutnumberSector(SOLDIERTYPE *pSoldier);
 
 INT32 AIPersonalRisk(SOLDIERTYPE *pSoldier);
 INT32 AIPersonalRiskTolerance(SOLDIERTYPE *pSoldier);
+INT8 AIAdvanceSupportModifier(SOLDIERTYPE *pSoldier, INT32 sTargetSpot = NOWHERE);
 UINT8 CountNearbyFriends( SOLDIERTYPE *pSoldier, INT32 sGridNo, UINT8 ubDistance );
 UINT8 CountNearbyNeutrals(SOLDIERTYPE *pSoldier, INT32 sGridNo, INT16 sDistance);
 UINT8 CountFriendsInDirection(SOLDIERTYPE *pSoldier, UINT8 ubDirection, INT16 sDistance, BOOLEAN fCheckSight);
