0152f073a6d9d8da8340de7af00d51ca926bb076
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-11 02:36:43 +0200

    NCTH: delay close-range scope penalty like 1.13

--- Tactical/Weapons.cpp
+++ Tactical/Weapons.cpp
@@ -5649,10 +5649,10 @@ if (gGameExternalOptions.fUseNewCTHCalculation)
 		// factor in scopes under their range
 		if ( !pSoldier->IsValidAlternativeFireMode( ubAimTime, sGridNo ) )
 		{
-			if (fScopeMagFactor > 1.0 && iRange < (INT32)uiBestScopeRange)
+			if (fScopeMagFactor > 1.0 && iRange < (INT32)(uiBestScopeRange * gGameCTHConstants.AIM_TOO_CLOSE_THRESHOLD))
 			{
 				// Calculate how much penalty this scope should give at this range
-				FLOAT dScopePenaltyRatio = ((FLOAT)uiBestScopeRange / (FLOAT)iRange);
+				FLOAT dScopePenaltyRatio = ((FLOAT)uiBestScopeRange * gGameCTHConstants.AIM_TOO_CLOSE_THRESHOLD / (FLOAT)iRange);
 				FLOAT iScopePenalty = (FLOAT)(dScopePenaltyRatio * gGameCTHConstants.AIM_TOO_CLOSE_SCOPE * (fScopeMagFactor /2));
 				fAimModifier += iScopePenalty;
 			}
