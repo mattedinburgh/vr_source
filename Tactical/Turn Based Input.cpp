9c2472dc431006307d9146236042029fc1642c15
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-12 21:50:09 +0200

    Downed rescue: bind backslash to casualty drag

--- Tactical/Turn Based Input.cpp
+++ Tactical/Turn Based Input.cpp
@@ -2741,6 +2741,15 @@ void GetKeyboardInput( UINT32 *puiNewEvent )
 
 				break;
 
+			case '\\':
+				// 1.13-style rescue drag: face an adjacent downed ally and press backslash.
+				// Press again to release. Starting the drag stabilizes the casualty.
+				if ( gusSelectedSoldier != NOBODY )
+				{
+					ToggleDownedDragInFront( MercPtrs[gusSelectedSoldier] );
+				}
+				break;
+
 			case '/':
 				// Center to guy....
 				if ( gusSelectedSoldier != NOBODY )
