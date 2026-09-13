fa5bbaad743f7ee2b5512b32eda0cfefc2d2c5fe
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-13 03:26:38 +0200

    Expose NCTH hit logging hook

--- Utils/message.h
+++ Utils/message.h
@@ -127,6 +127,7 @@ void DisplayLastMessage( void );
 
 void BattleLogAddNCTHMiss( INT32 iBullet );
 void BattleLogAddNCTHBlocked( INT32 iBullet, UINT8 ubReason );
+void BattleLogAddNCTHHit( INT32 iBullet, UINT8 ubTargetID, INT16 sDamage );
 void BattleLogAddText( UINT16 usColor, STR16 pString );
 void BattleLogSetVisible( BOOLEAN fVisible );
 
