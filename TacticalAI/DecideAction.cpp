5b8a9cfaa9e543e95b421e0af7eaf00b0f81f457
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-13 03:09:00 +0200

    Keep covert inspection enemy-specific

--- TacticalAI/DecideAction.cpp
+++ TacticalAI/DecideAction.cpp
@@ -1969,7 +1969,7 @@ INT8 DecideActionYellow(SOLDIERTYPE *pSoldier)
 		}
 
 		// sevenfm: officer can come to inspect suspicious soldier
-		if ( HAS_SKILL_TRAIT(pSoldier, SQUADLEADER_NT) )
+		if ( pSoldier->bTeam == ENEMY_TEAM && HAS_SKILL_TRAIT(pSoldier, SQUADLEADER_NT) )
 		{
 			UINT8 ubPerson = GetClosestFlaggedSoldierID( pSoldier, VISION_RANGE, OUR_TEAM, SOLDIER_COVERT_SOLDIER | SOLDIER_COVERT_CIV, TRUE );
 
