5f37138fe74d48031e539ac944ab301776a6cb0b
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-14 12:45:55 +0200

    Expose formation command quality for amateur army behavior

--- Strategic/Strategic Operational AI.h
+++ Strategic/Strategic Operational AI.h
@@ -83,6 +83,7 @@ void VR_OnEnemyGroupRetreated( GROUP *pGroup );
 void VR_HourlyOperationalUpdate();
 void VR_ReportOperationalIntel( UINT8 ubSectorID, UINT8 ubConfidence );
 
+UINT8 VR_GetFormationCommandQuality( GROUP *pGroup );
 INT32 VR_ScoreOperationalTarget( GROUP *pGroup, UINT8 ubSectorID, VR_OPERATIONAL_SCORE *pBreakdown );
 UINT8 VR_FindBestOperationalTarget( GROUP *pGroup, INT32 *piBestScore );
 
