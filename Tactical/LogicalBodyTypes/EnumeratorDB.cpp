8166bdff49a399260f2005c67ec75282dd589444
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-14 21:54:24 +0200

    Harden logical body enum registration in release builds

--- Tactical/LogicalBodyTypes/EnumeratorDB.cpp
+++ Tactical/LogicalBodyTypes/EnumeratorDB.cpp
@@ -20,6 +20,9 @@ void EnumeratorDB::AddEnum(std::string enumName, int num, char* strEnumerators, 
 	// TODO: remove output to cout Log this event somehow not just do the assertion!
 	if (numSemiColons + 1 != num) {
 		std::cout << enumName << " expected " << num << " got " << numSemiColons + 1 << std::endl;
+		delete[] str;
+		assert(numSemiColons + 1 == num);
+		return;
 	}
 	assert(numSemiColons + 1 == num);
 
@@ -31,6 +34,12 @@ void EnumeratorDB::AddEnum(std::string enumName, int num, char* strEnumerators, 
 	va_list vl;
 	va_start(vl, strEnumerators);
 	for (int i = 0; i < num; i++) {
+		if (token == NULL) {
+			std::cout << enumName << " tokenization failed at enumerator " << i << " of " << num << std::endl;
+			enumerations.erase(enumName);
+			assert(token != NULL);
+			break;
+		}
 		enumerations[enumName][token] = va_arg(vl, int);
 		token = strtok(NULL, ", ");
 	}
