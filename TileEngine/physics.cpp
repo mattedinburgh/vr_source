84b65c5ad915abde905db359a12631326d3fb95a
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-16 00:21:03 +0200

    Port 1.13 CTH dispersion and grenade rolling

--- TileEngine/physics.cpp
+++ TileEngine/physics.cpp
@@ -2092,6 +2092,96 @@ void CalculateLaunchItemBasicParams(SOLDIERTYPE *pSoldier, OBJECTTYPE *pItem, IN
 	(*pdDegrees) = dDegrees;
 }
 
+BOOLEAN GrenadeRollingPossible(SOLDIERTYPE *pSoldier, INT32 sGridNo, INT16 *sXPos, INT16 *sYPos)
+{
+	if (!(pSoldier->bWeaponMode == WM_ATTACHED_GL || pSoldier->bWeaponMode == WM_ATTACHED_GL_BURST || pSoldier->bWeaponMode == WM_ATTACHED_GL_AUTO))
+	{
+		UINT8 ubDirection = GetDirectionFromGridNo(sGridNo, pSoldier);
+		if (ubDirection % 2 == 1)
+		{
+			return FALSE;
+		}
+		INT32 sTestGridNo = NewGridNo(pSoldier->sGridNo, DirectionInc(ubDirection));
+
+		if (gubWorldMovementCosts[sTestGridNo][ubDirection][pSoldier->pathing.bLevel] == TRAVELCOST_WALL)
+		{
+			BOOLEAN obstacle = FALSE;
+			INT16 newDir = (ubDirection != 2 && ubDirection != 6) ? EAST : SOUTH;
+			INT32 newLoc = NewGridNo(pSoldier->sGridNo, DirectionInc(newDir));
+			STRUCTURE *pStruct = gpWorldLevelData[newLoc].pStructureHead;
+			while (pStruct)
+			{
+				if ((pStruct->fFlags & STRUCTURE_ANYDOOR) && (pStruct->fFlags & STRUCTURE_OPEN))
+				{
+					if (gubWorldMovementCosts[newLoc][newDir][pSoldier->pathing.bLevel] >= 220)
+						return FALSE;
+					ConvertGridNoToCenterCellXY(newLoc, sXPos, sYPos);
+					return TRUE;
+				}
+				pStruct = pStruct->pNext;
+			}
+			newLoc = NewGridNo(sTestGridNo, DirectionInc(newDir));
+			pStruct = gpWorldLevelData[newLoc].pStructureHead;
+			while (pStruct)
+			{
+				if ((pStruct->fFlags & STRUCTURE_ANYDOOR) && (pStruct->fFlags & STRUCTURE_OPEN))
+				{
+					ConvertGridNoToCenterCellXY(newLoc, sXPos, sYPos);
+					return TRUE;
+				}
+				else if ((pStruct->fFlags & STRUCTURE_OBSTACLE))
+				{
+					obstacle = TRUE;
+				}
+				pStruct = pStruct->pNext;
+			}
+			if (!obstacle)
+			{
+				ConvertGridNoToCenterCellXY(newLoc, sXPos, sYPos);
+				return TRUE;
+			}
+
+			obstacle = FALSE;
+			newDir = (ubDirection != 2 && ubDirection != 6) ? WEST : NORTH;
+			newLoc = NewGridNo(pSoldier->sGridNo, DirectionInc(newDir));
+			pStruct = gpWorldLevelData[newLoc].pStructureHead;
+			while (pStruct)
+			{
+				if ((pStruct->fFlags & STRUCTURE_ANYDOOR) && (pStruct->fFlags & STRUCTURE_OPEN))
+				{
+					if (gubWorldMovementCosts[newLoc][newDir][pSoldier->pathing.bLevel] >= 220)
+						return FALSE;
+					ConvertGridNoToCenterCellXY(newLoc, sXPos, sYPos);
+					return TRUE;
+				}
+				pStruct = pStruct->pNext;
+			}
+			newLoc = NewGridNo(sTestGridNo, DirectionInc(newDir));
+			pStruct = gpWorldLevelData[newLoc].pStructureHead;
+			while (pStruct)
+			{
+				if ((pStruct->fFlags & STRUCTURE_ANYDOOR) && (pStruct->fFlags & STRUCTURE_OPEN))
+				{
+					ConvertGridNoToCenterCellXY(newLoc, sXPos, sYPos);
+					return TRUE;
+				}
+				else if ((pStruct->fFlags & STRUCTURE_OBSTACLE))
+				{
+					obstacle = TRUE;
+				}
+				pStruct = pStruct->pNext;
+			}
+			if (!obstacle)
+			{
+				ConvertGridNoToCenterCellXY(newLoc, sXPos, sYPos);
+				return TRUE;
+			}
+		}
+	}
+	return FALSE;
+}
+
+
 BOOLEAN CalculateLaunchItemChanceToGetThrough( SOLDIERTYPE *pSoldier, OBJECTTYPE *pItem, INT32 sGridNo, UINT8 ubLevel, INT16 sEndZ,	INT32 *psFinalGridNo, BOOLEAN fArmed, INT8 *pbLevel, BOOLEAN fFromUI )
 {
 	FLOAT				dForce, dDegrees;
@@ -2111,6 +2201,11 @@ BOOLEAN CalculateLaunchItemChanceToGetThrough( SOLDIERTYPE *pSoldier, OBJECTTYPE
 	// Get XY from gridno
 	ConvertGridNoToCenterCellXY( sGridNo, &sDestX, &sDestY );
 	ConvertGridNoToCenterCellXY( pSoldier->sGridNo, &sSrcX, &sSrcY );
+	if (GrenadeRollingPossible(pSoldier, sGridNo, &sSrcX, &sSrcY))
+	{
+		dForce /= 2;
+	}
+
 
 	// Set position
 	vPosition.x = sSrcX;
@@ -2214,7 +2309,7 @@ FLOAT CalculateSoldierMaxForce( SOLDIERTYPE *pSoldier, FLOAT dDegrees , OBJECTTY
 }
 
 
-void CalculateLaunchItemParamsForThrow( SOLDIERTYPE *pSoldier, INT32 sGridNo, UINT8 ubLevel, INT16 sEndZ, OBJECTTYPE *pItem, INT8 bMissBy, UINT8 ubActionCode, UINT32 uiActionData )
+void CalculateLaunchItemParamsForThrow( SOLDIERTYPE *pSoldier, INT32 sGridNo, UINT8 ubLevel, INT16 sEndZ, OBJECTTYPE *pItem, UINT32 uiHitChance, UINT8 ubActionCode, UINT32 uiActionData, UINT16 usItemNum )
 {
 	FLOAT				dForce, dDegrees;
 	INT16				sDestX, sDestY, sSrcX, sSrcY;
@@ -2224,7 +2319,6 @@ void CalculateLaunchItemParamsForThrow( SOLDIERTYPE *pSoldier, INT32 sGridNo, UI
 	UINT16			usLauncher;
 	INT16				sStartZ;
 	INT8		bMinMissRadius, bMaxMissRadius, bMaxRadius;
-	FLOAT		fScale;
 
 	DebugMsg (TOPIC_JA2,DBG_LEVEL_3,"CalculateLaunchItemParamsForThrow");
 
@@ -2236,74 +2330,29 @@ void CalculateLaunchItemParamsForThrow( SOLDIERTYPE *pSoldier, INT32 sGridNo, UI
 		fArmed = TRUE;
 	}
 
-	if ( bMissBy < 0 )
+	// Modern 1.13: convert actual CTH into a continuous landing-dispersion envelope.
+	// Mortars deliberately use a wider configurable miss radius than hand/launcher grenades.
+	if (Item[usItemNum].mortar)
 	{
-		// then we hit!
-		bMissBy = 0;
-
-		// SANDRO - new merc records
-		if( pSoldier->bTeam == 0 && pSoldier->ubProfile != NO_PROFILE )
-		{
-			gMercProfiles[ pSoldier->ubProfile ].records.usShotsHit++;
-		}
+		bMaxRadius = gItemSettings.usMissMaxRadiusMortar;
+	}
+	else
+	{
+		bMaxRadius = gItemSettings.usMissMaxRadiusGrenade;
 	}
 
-	//if ( 0 )
-	if ( bMissBy > 0 )
-	{
-		// Max the miss variance
-		if ( bMissBy > MAX_MISS_BY )
-		{
-			bMissBy = MAX_MISS_BY;
-		}
+	uiHitChance = __min((UINT32)100, uiHitChance);
+	bMinMissRadius = (INT8)((FLOAT)bMaxRadius * (10.0f - sqrt((FLOAT)uiHitChance)) / 10.0f);
+	bMaxMissRadius = (INT8)(bMaxRadius * (100 - uiHitChance) / 100);
 
-		// Min the miss varience...
-		if ( bMissBy < MIN_MISS_BY )
-		{
-			bMissBy = MIN_MISS_BY;
-		}
+	// Small luck variation, matching the modern 1.13 landing model.
+	bMinMissRadius = __max((bMinMissRadius - (INT8)Random(2)), 0);
+	bMinMissRadius = __min((bMinMissRadius + (INT8)Random(2)), bMaxRadius);
+	bMaxMissRadius = __max((bMaxMissRadius - (INT8)Random(2)), 0);
+	bMaxMissRadius = __min((bMaxMissRadius + (INT8)Random(2)), bMaxRadius);
+	bMinMissRadius = __min(bMinMissRadius, bMaxMissRadius);
 
-		// Adjust position, force, angle
-#ifdef JA2TESTVERSION
-		ScreenMsg( FONT_MCOLOR_LTYELLOW, MSG_TESTVERSION, L"Throw miss by: %d", bMissBy );
-#endif
-
-		// Default to max radius...
-		bMaxRadius = 5;
-
-		// scale if pyth spaces away is too far
-		if ( PythSpacesAway( sGridNo, pSoldier->sGridNo ) < ( (float)bMaxRadius / (float)1.5 ) )
-		{
-			bMaxRadius = PythSpacesAway( sGridNo, pSoldier->sGridNo ) / 2;
-		}
-
-
-		// Get radius
-		fScale = ( (float)bMissBy / (float) MAX_MISS_BY );
-
-		bMaxMissRadius = (INT8)( bMaxRadius * fScale );
-
-		// Limit max radius...
-		if ( bMaxMissRadius > 4 )
-		{
-			bMaxMissRadius = 4;
-		}
-
-
-		bMinMissRadius = bMaxMissRadius - 1;
-
-		if ( bMinMissRadius < 2 )
-		{
-			bMinMissRadius = 2;
-		}
-
-		if ( bMaxMissRadius < bMinMissRadius )
-		{
-			bMaxMissRadius = bMinMissRadius;
-		}
-
-		sGridNo = RandomGridFromRadius( sGridNo, bMinMissRadius, bMaxMissRadius );
-	}
+	sGridNo = RandomGridFromRadius(sGridNo, bMinMissRadius, bMaxMissRadius);
 
 	// Get basic launch params...
 	CalculateLaunchItemBasicParams( pSoldier, pItem, sGridNo, ubLevel, sEndZ, &dForce, &dDegrees, &sFinalGridNo, fArmed );
@@ -2311,6 +2360,11 @@ void CalculateLaunchItemParamsForThrow( SOLDIERTYPE *pSoldier, INT32 sGridNo, UI
 	// Get XY from gridno
 	ConvertGridNoToCenterCellXY( sGridNo, &sDestX, &sDestY );
 	ConvertGridNoToCenterCellXY( pSoldier->sGridNo, &sSrcX, &sSrcY );
+
+	if (GrenadeRollingPossible(pSoldier, sGridNo, &sSrcX, &sSrcY))
+	{
+		dForce /= 2;
+	}
 
 	// OK, get direction normal
 	vDirNormal.x = (float)(sDestX - sSrcX);
