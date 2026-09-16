4fcda00a037f2826c9edc78b854b21a44ffe7320
mattedinburgh <Mateusz.Bialy@outlook.com>
2026-09-16 03:52:08 +0200

    Restore attachment roll parameter in inventory API

--- Tactical/Inventory Choosing.h
+++ Tactical/Inventory Choosing.h
@@ -58,7 +58,7 @@ void InitArmyGunTypes(void);
 //of equipment to choose from.
 void GenerateRandomEquipment( SOLDIERCREATE_STRUCT *pp, INT8 bSoldierClass, INT8 bEquipmentRating);
 
-void ChooseWeaponForSoldierCreateStruct( SOLDIERCREATE_STRUCT *pp, INT8 bWeaponClass, INT8 bAmmoClips, INT8 bAttachClass );
+void ChooseWeaponForSoldierCreateStruct( SOLDIERCREATE_STRUCT *pp, INT8 bWeaponClass, INT8 bAmmoClips, INT8 bAttachClass, BOOLEAN fAttachment );
 
 void ChooseGrenadesForSoldierCreateStruct( SOLDIERCREATE_STRUCT *pp, INT8 bGrenades, INT8 bGrenadeClass, BOOLEAN fGrenadeLauncher );
 
